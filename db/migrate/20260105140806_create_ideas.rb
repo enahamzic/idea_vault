4be67df8500e54f5d733796a1f58875a82bc3c8a
enahamzic# <enahamzic16@gmail.com>
2026-01-05 15:08:35 +0100

    Add Ideas resource with CRUD functionality and associated views, models, and tests

---
+++ db/migrate/20260105140806_create_ideas.rb
@@ -0,0 +1,12 @@
+class CreateIdeas < ActiveRecord::Migration[8.1]
+  def change
+    create_table :ideas do |t|
+      t.string :title
+      t.text :description
+      t.integer :rating
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_01_05_140438) do
+ActiveRecord::Schema[8.1].define(version: 2026_01_05_140806) do
+  create_table "ideas", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.text "description"
+    t.integer "rating"
+    t.string "title"
+    t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_ideas_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "email", default: "", null: false
@@ -22,4 +32,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_01_05_140438) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "ideas", "users"
 end
